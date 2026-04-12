6a7561f53a690eecc3ce5acfcb2340eb13f6a7cc
Roshan <roshanshri239@gmail.com>
2026-04-12 17:21:18 +0530

    gvhg

--- assets/js/script.js
+++ assets/js/script.js
@@ -67,7 +67,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Jigar Sable";
+            document.title = "Portfolio | Roshan";
             $("#favicon").attr("href", "assets/images/favicon.png");
         }
         else {
